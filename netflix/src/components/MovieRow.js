37c284d23c16f5de0dedbdfaf48fc47499887f71
KAUANSANTOS <kauanauan71@gmail.com>
2021-10-04 22:02:20 -0300

    Estilização

--- netflix/src/components/MovieRow.js
+++ netflix/src/components/MovieRow.js
@@ -5,13 +5,16 @@ import './MovieRow.css'
 // componente que irar retornar baisco com o return ()
 export default ({title, items}) => {
     return(
-        <div>
+        <div className ='movieRow'>
             <h2>{title}</h2>
             <div className ='movie-list-area'>
-                {items.results.length>0 && items.results.map((item,key)=>(
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-
-                ))}
+                <div className='movie-lista'>
+                    {items.results.length>0 && items.results.map((item,key)=>(
+                        <div className='movie-list-item'>
+                        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
         
