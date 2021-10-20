b4395704922bc986798dc89050b6c6c65cb41631
KAUANSANTOS <kauanauan71@gmail.com>
2021-10-19 23:14:59 -0300

    Atualizações
    Atualização de projeto , requisições

--- netflix/src/components/destaque.js
+++ netflix/src/components/destaque.js
@@ -3,7 +3,15 @@ import './Destaque.css'
 
 //mostrando o header do projeto através de componentes
 export default({item})=>{
+    let datas = new Date(item.first_air_date)
+    let genres = []
+    for(let i in genres){
+        genres.push(item.genres[i].name)
+    }
+    console.log(item)
     return(
+
+      
         <section className='featured'style={{
             backgroundSize:'cover',
             backgroundPosition:'center',
@@ -18,16 +26,18 @@ export default({item})=>{
                         <div className='pontos'>
                             {item.vote_average} Pontos
                         </div>
-                        <div className='ano'>2099</div>
-                        <div className='temporada'>{item.number_of_seasons}Temporada{item.number_of_seasons!==1?'s':''}</div>
+                        <div className='ano'>{datas.getFullYear()}</div>
+                        <div className='temporada'>{item.number_of_seasons} Temporada{item.number_of_seasons!==1?'s':''}</div>
                     </div>
-                    <div className='descricao'>
-                        {item.overview}
-                    </div>
+                    <div className='descricao'>{item.overview}</div>
+                       
+                    
                     <div className='buttons'>
+                        <a href={`/watch/${item.id}`}>Assistir</a>
+                        <a href={`/list/add/${item.id}`}>+ Minha lista</a>
 
                     </div>
-                    <div className='generos'><strong>Generos:</strong>{}</div>
+                    <div className='generos'><strong>Generos:</strong>{genres.join(' , ')}</div>
 
 
                 </div>
